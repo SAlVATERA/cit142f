7bd076e5e0339a70a6b4683192a2962ae8af1290
cit142f-37@M307.UC-BCF.EDU.PH <cit142f-37@M307.UC-BCF.EDU.PH>
2022-09-21 17:45:34 +0800

    -BATCH UPLOAD

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 
-git remote set-url origin https://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@github.com/SALVATERA/cit142f.git
+git remote set-url origin https://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@github.com/SALVATERA/cit142f.git
 pause
 git add .
 git commit -m -"BATCH UPLOAD"
